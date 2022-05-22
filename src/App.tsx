c92b85a9aac19c61da0401ae11bcb10c2e1a058d
MinChan Lee <shigatsu970704@gmail.com>
2022-05-22 13:50:35 +0900

    [Feat] View3D 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
+import { AiOutlineExclamationCircle } from 'react-icons/ai';
+import View3D, { LoadingBar } from '@egjs/view3d';
+import '@egjs/view3d/css/view3d-bundle.min.css';
 import { Image } from './components/Image';
 
 function App() {
+  useEffect(() => {
+    new View3D('#wrapper-el', {
+      src: '/Chair.glb',
+      plugins: [new LoadingBar()],
+    });
+  }, []);
+
   return (
     <FlexColumn>
       <FlexCenter>
         <Logo src="/logo.png" alt="Logo" width="100rem" />
       </FlexCenter>
-      <FlexCenter></FlexCenter>
+      <Background>
+        <ThreeDimentionContainer>
+          <>
+            <AiOutlineExclamationCircle style={{ alignSelf: 'end', cursor: 'pointer' }} />
+            <Viewer id="wrapper-el">
+              <canvas className="view3d-canvas"></canvas>
+            </Viewer>
+          </>
+        </ThreeDimentionContainer>
+      </Background>
     </FlexColumn>
   );
 }
@@ -17,7 +36,7 @@ export default App;
 
 const FlexCenter = styled.div`
   display: flex;
-  margin: 0 auto;
+  justify-content: center;
 `;
 
 const FlexColumn = styled.div`
@@ -27,4 +46,24 @@ const FlexColumn = styled.div`
 
 const Logo = styled(Image)`
   margin: 0.8rem 0 1.2rem 0;
+`;
+
+const Background = styled(FlexCenter)`
+  background-image: url('/background.png');
+  background-size: cover;
+  background-position: center;
+  width: 100vw;
+  height: 70vh;
+  align-items: center;
+`;
+
+const ThreeDimentionContainer = styled(FlexColumn)`
+  width: 50%;
+  height: 70%;
+`;
+
+const Viewer = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
 `;
